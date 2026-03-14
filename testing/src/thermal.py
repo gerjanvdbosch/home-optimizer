9fbb9c3e9f5840e9635cb6022195689e25eb0e34
gerjanvdbosch <gerjanvdbosch@live.nl>
2026-03-14 11:19:41 +0100

    Refactor

--- testing/src/thermal.py
+++ testing/src/thermal.py
@@ -1172,7 +1172,7 @@ class ComfortCostCalculator:
         avg_cop_ufh: float = 3.5,
         avg_cop_dhw: float = 2.5,
         recovery_hours_room: float = 4.0,
-        recovery_hours_dhw: float = 2.0,
+        recovery_hours_dhw: float = 1.0,
     ):
         self.C_room = C_room
         self.C_tank = C_tank
