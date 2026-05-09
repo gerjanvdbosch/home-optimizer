aec1b05941a693caeb3a9017dcd1fb81a5b26faa
gerjanvdbosch <gerjanvdbosch@live.nl>
2026-05-09 08:51:30 +0200

    Refactor

--- src/home_optimizer/features/modeling/room/__init__.py
+++ src/home_optimizer/features/modeling/room/__init__.py
@@ -1,10 +1,16 @@
 from .arx import (
+    RoomArxConfig,
+    RoomArxModel,
+    RoomArxTrainer,
     fit_room_arx_model,
     predict_next_room_arx_temperature,
     simulate_room_arx_horizon,
 )
 
 __all__ = [
+    "RoomArxConfig",
+    "RoomArxModel",
+    "RoomArxTrainer",
     "fit_room_arx_model",
     "predict_next_room_arx_temperature",
     "simulate_room_arx_horizon",
