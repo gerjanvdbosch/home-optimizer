08c5920b77d759e1f5571afed08d613c6cce9a85
gerjanvdbosch <gerjanvdbosch@live.nl>
2026-07-17 21:10:02 +0200

    Refactor

--- src/web/server.py
+++ src/web/server.py
@@ -1,16 +1,29 @@
-from fastapi import FastAPI
+from pathlib import Path
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from app.settings import load_settings
 from infrastructure.influx import InfluxDatabase
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="src/web/static"), name="static")
+
 settings = load_settings()
 db = InfluxDatabase(settings)
 
+templates = Jinja2Templates(directory=str(Path(__file__).parent / "templates"))
 
-@app.get("/")
-async def dashboard():
-    return {"ok"}
+
+@app.get("/", response_class=HTMLResponse)
+async def dashboard(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="dashboard.html",
+        context={},
+    )
 
 
 @app.get("/api/update")
