76a05b6a3e44c76a1cf058033355aea8f50b70ad
gerjanvdbosch <gerjanvdbosch@live.nl>
2026-07-18 22:30:41 +0200

    Refactor

--- src/infrastructure/storage.py
+++ src/infrastructure/storage.py
@@ -18,7 +18,6 @@ class JsonStorage:
         temp_path.write_text(
             json.dumps(
                 data,
-                indent=2,
                 default=str,
             ),
             encoding="utf-8",
