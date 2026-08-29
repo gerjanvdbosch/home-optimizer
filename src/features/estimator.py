97d43f26fe7717b897c87e572c975437d0a540db
gerjanvdbosch <gerjanvdbosch@live.nl>
2026-08-29 11:51:27 +0200

    Refactor

--- src/features/estimator.py
+++ src/features/estimator.py
@@ -1,14 +1,18 @@
 import logging
-from dataclasses import dataclass
 
 logger = logging.getLogger(__name__)
 
 
-@dataclass
 class DisturbanceEstimator:
-    alpha: float = 0.7
-    max: float = 2000.0
-    min: float = 50.0
+    def __init__(
+        self,
+        alpha: float = 0.7,
+        max: float = 2000.0,
+        min: float = 50.0,
+    ) -> None:
+        self.alpha = alpha
+        self.max = max
+        self.min = min
 
     def estimate(
         self,
